e63f9ecced866e1eb1b275c598769258474d9601
Timothy Axford <t.axford2@yahoo.com>
2023-04-07 09:19:52 -0400

    Created a third method example

--- WriteIntoText.Net5.0/Program.cs
+++ WriteIntoText.Net5.0/Program.cs
@@ -7,16 +7,29 @@ namespace WriteIntoText.Net5._0
     {
         static void Main(string[] args)
         {
-            string[] lines = { "250", "242", "240" };
+            //Method 1
+            string[] lines = { "First 250", "Second 242", "Third 240" };
 
             File.WriteAllLines(@"C:\Users\txa334\Desktop\Assets\highscores.txt", lines);
 
+            /*//Method 2
             Console.WriteLine("Please give the file a name");
-
             string fileName = Console.ReadLine();
+            Console.WriteLine("Please enter the text for the file");
             string input = Console.ReadLine();
-            File.WriteAllText(@"C:\Users\txa334\Desktop\Assets" + fileName + ".txt" , input);
+            File.WriteAllText(@"C:\Users\txa334\Desktop\Assets" + fileName + ".txt" , input);*/
 
+            //Method3
+            using (StreamWriter file = new StreamWriter(@"C:\Users\txa334\Desktop\Assets\myText.txt"))
+            {
+                foreach(string line in lines)
+                {
+                    if (line.Contains("Third"))
+                    {
+                        file.WriteLine(line);
+                    }
+                }
+            }
         }
     }
 }
